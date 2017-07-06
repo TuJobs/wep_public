9fabcb6873cf61ae9d844f04cbe426cc190c628a
Tu Jobs <nguyenvantu.epu@gmail.com>
2017-07-06 10:23:23 +0700

    add follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :active_relationships, class_name: Relationship.name,
     foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name:  Relationship.name,
+    foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token
 
@@ -44,5 +48,17 @@ class User < ApplicationRecord
     following_ids = "SELECT followed_id FROM relationships
       WHERE  follower_id = :user_id"
     Post.load_feed id, following_ids
+  end
+
+  def follow other_user
+    following << other_user
+  end
+
+  def unfollow other_user
+    following.delete other_user
+  end
+
+  def following? other_user
+    following.include? other_user
   end
 end
